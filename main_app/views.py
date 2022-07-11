7482b4cf954cecf58eabe9666690f6b88d639456
Anasemos Kassahun <anasemosaka@gmail.com>
2022-07-11 12:18:39 -0400

    blog page added with seed data

--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,20 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+
+class Blog:
+    def __init__(self, name, location, description,):
+        self.name = name
+        self.location = location
+        self.description = description
+
+
+blogs = [
+    Blog('Anasemos', 'Cancun, Mexico', 'View of Tulum')
+]
+
+
+def blogs_index(request):
+    return render(request, 'blogs/index.html', {'blogs': blogs})
